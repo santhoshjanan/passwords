e36ad9ced01ad7460928081463327856dfa40184
Santhosh Janardhanan <santhoshj@gmail.com>
2024-06-10 00:04:48 -0400

    formatting code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,24 @@
 <script>
-  import "../app.css";
+	import '../app.css';
 </script>
+
 <svelte:head>
 	<title>Passwords</title>
 </svelte:head>
 
 <div class="font-mono">
-<slot />
+	<slot />
 </div>
 <footer class="fixed bottom-0 left-0 w-full bg-green-500 text-white p-4">
 	<div class="flex w-full justify-between">
-	<p><span class="font-mono">2024 &copy; Made in New Jersey with </span>❤️ <span class="font-mono"> and Svelte!</span></p>
-	<p class="transition duration-300 ease-in-out hover:ease-out hover:scale-105 hover:italics hover:underline font-mono"><a href="mailto:eagles_jokers0o@icloud.com">Do you want another feature? Reach Me!</a></p>
+		<p>
+			<span class="font-mono">2024 &copy; Made in New Jersey with </span>❤️
+			<span class="font-mono"> and Svelte!</span>
+		</p>
+		<p
+			class="transition duration-300 ease-in-out hover:ease-out hover:scale-105 hover:italics hover:underline font-mono"
+		>
+			<a href="mailto:eagles_jokers0o@icloud.com">Do you want another feature? Reach Me!</a>
+		</p>
 	</div>
 </footer>
